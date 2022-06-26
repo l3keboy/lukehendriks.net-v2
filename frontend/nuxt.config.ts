d18dd69741f7c9c4bbf946873f6545442648816e
l3keboy <66973798+l3keboy@users.noreply.github.com>
2022-06-26 17:07:33 +0200

    Add TailwindCSS

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,5 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    css: ["@/assets/styles/styles.css"],
+    build: {
+      postcss: {
+        postcssOptions: require("./postcss.config.js"),
+      },
+    },
 })
