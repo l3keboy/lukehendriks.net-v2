a622cdbac7c22b7b4f2e6c7b026cc7299f0e737c
l3keboy <66973798+l3keboy@users.noreply.github.com>
2024-10-20 19:42:58 +0200

    Update

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,14 +2,17 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: [
-      "@/assets/styles/styles.css",
-      "@/assets/styles/scrollbar.css",
-    ],
-    postcss: {
-      plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-      },
+  css: [
+    "@/assets/styles/styles.css",
+    "@/assets/styles/scrollbar.css",
+  ],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
+  },
+
+  compatibilityDate: "2024-10-20",
 })
